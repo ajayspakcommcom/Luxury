d54820ae9be5625e50e4e44dddef197e7304c48d
Ajay Mahavir Vishwakarma <ajayspak@spakcomm.com>
2024-06-11 15:24:03 +0530

    changes

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -4,13 +4,15 @@ import CarouselItem from './CarouselItem';
 
 const Carousel = (props) => {
 
+
+
     return (
 
         <div id={props.id} className="carousel slide" data-ride="carousel">
 
-            {/* <ol className="carousel-indicators">
+            {false && <ol className="carousel-indicators">
                 {props.carouselData.map((item, indx) => { return <CarouselIndicator key={item.id} dataTarget={props.id} dataSlideTo={item.id} class={indx === 0 ? 'active' : ''} /> })}
-            </ol> */}
+            </ol>}
 
             <div className="carousel-inner">
 
@@ -21,16 +23,24 @@ const Carousel = (props) => {
                 }
 
                 <a className="left carousel-control" href={`#${props.id}`} data-slide="prev">
-                    {/* <span className="glyphicon glyphicon-chevron-left"></span>
-                    <span className="sr-only">{props.prevText}</span> */}
+                    {false && <>
+                        <span className="glyphicon glyphicon-chevron-left"></span>
+                        <span className="sr-only">{props.prevText}</span>
+                    </>
+                    }
                     <Icons icon='left-arrow' />
                 </a>
-                <a className="right carousel-control" href={`#${props.id}`} data-slide="next">
-                    {/* <span className="glyphicon glyphicon-chevron-right"></span>
-                    <span className="sr-only">{props.nextText}</span> */}
+                {true && <a className="right carousel-control" href={`#${props.id}`} data-slide="next">
+                    {
+                        false && <>
+                            <span className="glyphicon glyphicon-chevron-right"></span>
+                            <span className="sr-only">{props.nextText}</span>
+                        </>
+                    }
                     <Icons icon='right-arrow' />
-                </a>
+                </a>}
             </div>
+
         </div>
     );
 
--- src/components/Carousel/CarouselItem.js
+++ src/components/Carousel/CarouselItem.js
@@ -2,12 +2,15 @@ import React from 'react';
 import parse from "html-react-parser";
 
 const CarouselItem = (props) => {
+
+    console.log('props', props)
+
     return (
         <>
             <div className={props.class}>
-                <img src={require(`../../content/images/carousel/${props.src}`)} alt={props.smallText} />
+                <img src={`carousel/${props.src}`} alt={props.smallText} />
                 <div className="carousel-caption">
-                    <h3>{props.smallText}</h3>
+                    <h3>{parse(props.smallText)}</h3>
                     <p>{parse(props.bigText)}</p>
                 </div>
             </div>
